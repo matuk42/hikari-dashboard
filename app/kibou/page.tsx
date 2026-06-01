ec4010d1432aaf914f416506c5ee9eb33216f113
matuk42 <matypleva1@gmail.com>
2026-06-01 22:03:11 +0200

    auto: save changes

--- app/kibou/page.tsx
+++ app/kibou/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
+import React, { useState, useEffect, useCallback } from 'react'
 import Link from 'next/link'
-import { useState, useEffect, useCallback } from 'react'
 import {
   LineChart,
   Line,
