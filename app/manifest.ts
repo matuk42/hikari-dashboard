34d12216d1babc671b5de2612b8113d064dfc395
matuk42 <matypleva1@gmail.com>
2026-06-01 18:41:37 +0200

    auto: save changes

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,6 +11,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#F59E0B',
     icons: [
       {
+        src: '/icon-192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
         src: '/icon.png',
         sizes: '512x512',
         type: 'image/png',
